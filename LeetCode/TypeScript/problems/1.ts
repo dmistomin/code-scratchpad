68b4df449aefcce7a4c78e3e8ccc2cc4419e8268
Dmitriy Istomin <distomin+git@pm.me>
2024-07-31 16:10:29 -0400

    add LeetCode Two Sum problem solution

---
+++ LeetCode/TypeScript/problems/1.ts
@@ -0,0 +1,20 @@
+function twoSum(nums: Array<number>, target: number): Array<number> {
+  const visited = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const n = nums[i];
+    const remainder = target - n;
+
+    if (remainder in visited) {
+      return [visited[remainder], i];
+    }
+
+    visited[n] = i;
+  }
+
+  return [];
+}
+
+console.log('nums = [3,3], target = 6: ', twoSum([3, 3], 6));
+console.log('nums = [3,2,4], target = 6', twoSum([3, 2, 4], 6));
+console.log('nums = [2,7,11,15], target = 9', twoSum([2, 7, 11, 15], 9));
