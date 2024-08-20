b1a8d2bea7746fba071427ee28a4aeca8d85128f
Dmitriy Istomin <distomin+git@pm.me>
2024-08-20 17:52:34 -0400

    add TS 'types' file

---
+++ react/react-practice-exercises/infinite-pokemon-list/src/types.d.ts
@@ -0,0 +1,11 @@
+type PokemonDetail = {
+  id: number
+  name: string
+  sprites: {
+    back_default: string
+    back_shiny: string
+    front_default: string
+    front_shiny: string
+  }
+  weight: number
+}
